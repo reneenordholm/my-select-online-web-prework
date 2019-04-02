fb50f8e232775d9ed9e2c7c3444bae7ced5e227d
Renee Nordholm <reneenordholm@gmail.com>
2019-04-02 23:09:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
 def my_select(collection)
  i = 0
  nums = []
-  while i < collection.length
+
   nums do |num|
-    collection << num.select { |num|  num.even?  }
+    yield(collection << num.select { |num|  num.even?  })
     i = i + 1
  end
  nums
