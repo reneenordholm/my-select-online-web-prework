c80bdaa38374bd778413b2b1ea8c7d89bb77f83b
Renee Nordholm <reneenordholm@gmail.com>
2019-04-02 22:54:42 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,3 @@
 def my_select(collection)
-  x = 0
- new_arr = []
-     while x < collection.length
-         new_arr << yield(collection[x].select { |num|  num.even?  })
-         x += 1
-     end
-     new_arr
- end
+
 end
